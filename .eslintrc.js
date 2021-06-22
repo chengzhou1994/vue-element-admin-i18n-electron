7bd542f5d7af3f8435e0c5350b8dfbf43277fb5a
成舟 <1738248438@qq.com>
2021-06-23 00:13:21 +0800

    🎉 初次提交

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'off',
+    'no-useless-escape': 'off'
+  }
+  // add your custom rules here
+  // it is base on https://github.com/vuejs/eslint-config-vue
+}
